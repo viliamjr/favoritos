e7371a3609358394f0c0b67b4c79824ce5f645d3
Viliam Dias <viliamjr@gmail.com>
2016-04-01 16:57:49 -0300

    Flags da linha de comando para tratar login.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 	"strconv"
@@ -11,6 +12,11 @@ import (
 )
 
 func main() {
+
+	endereço := flag.String("e", ":8080", "endereço [IP:PORTA] a ser usado")
+	usuario := flag.String("u", "admin", "nome do usuário")
+	senha := flag.String("s", "foobar", "senha do usuário")
+	flag.Parse()
 
 	// Configurando banco de dados
 	var err error
@@ -30,7 +36,7 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 
 	// Habilitando esquema de autorização simples
-	auth := r.Group("/", gin.BasicAuth(gin.Accounts{"admin": "foobar"}))
+	auth := r.Group("/", gin.BasicAuth(gin.Accounts{*usuario: *senha}))
 
 	auth.GET("/", func(c *gin.Context) {
 
@@ -95,7 +101,7 @@ func main() {
 		})
 	})
 
-	r.Run(":8080")
+	r.Run(*endereço)
 }
 
 func construirLink(c *gin.Context) *Link {
